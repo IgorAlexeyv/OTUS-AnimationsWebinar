a44b82f71e45dd6456b797a859b7290480919be0
PC <anikin13@gmail.com>
2024-12-22 18:54:44 +0400

    2. Transitions: enlarge image

--- app/src/main/java/com/otus/animations/FirstFragment.kt
+++ app/src/main/java/com/otus/animations/FirstFragment.kt
@@ -1,10 +1,14 @@
 package com.otus.animations
 
 import android.os.Bundle
+import android.transition.ChangeBounds
+import android.transition.ChangeImageTransform
 import android.transition.TransitionManager
+import android.transition.TransitionSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.otus.animations.databinding.FragmentFirstBinding
 
@@ -13,7 +17,7 @@ class FirstFragment : Fragment() {
     private var _binding: FragmentFirstBinding? = null
     private val binding get() = _binding!!
 
-    private var textIsVisible = false
+    private var isExpanded = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,11 +32,21 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            TransitionManager.beginDelayedTransition(binding.transitionsContainer)
-            textIsVisible = !textIsVisible
-            binding.textviewFirst.visibility = if (textIsVisible) View.VISIBLE else View.GONE
-            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        with(binding.imageView) {
+            setOnClickListener {
+                isExpanded = !isExpanded
+                TransitionManager.beginDelayedTransition(
+                    binding.transitionsContainer, TransitionSet()
+                        .addTransition(ChangeBounds())
+                        .addTransition(ChangeImageTransform())
+                )
+
+                val params: ViewGroup.LayoutParams = layoutParams
+                params.height = if (isExpanded) ViewGroup.LayoutParams.MATCH_PARENT else ViewGroup.LayoutParams.WRAP_CONTENT
+                layoutParams = params
+                scaleType = if (isExpanded) ImageView.ScaleType.CENTER_CROP else ImageView.ScaleType.FIT_CENTER
+                //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
         }
     }
 
