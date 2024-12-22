3c9849194a6373b4f259e3259d1ddb97087ae619
PC <anikin13@gmail.com>
2024-12-22 20:02:53 +0400

    3. ObjectAnimator + View.animate

--- app/src/main/java/com/otus/animations/MainActivity.kt
+++ app/src/main/java/com/otus/animations/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.otus.animations
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
 import com.otus.animations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,16 +19,84 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private var isExpanded = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setActionBar()
+        setFAB()
+    }
+
+    private fun setFAB() {
+        setInitialState()
+
 
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+            if (isExpanded) {
+                collapseFab()
+            } else {
+                expandFAB()
+            }
+        }
+    }
+
+    private fun setInitialState() {
+        binding.transparentBackground.apply {
+            alpha = 0f
+        }
+        binding.optionContainer.apply {
+            alpha = 0f
+            isClickable = false
+        }
+    }
+
+    private fun expandFAB() {
+        isExpanded = true
+        ObjectAnimator.ofFloat(binding.plusImageview, "rotation", 0f, 225f).start()
+        ObjectAnimator.ofFloat(binding.optionContainer, "translationY", -130f).start()
+
+        with(binding.optionContainer) {
+            animate().alpha(1f).setDuration(300).setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    isClickable = true
+                    setOnClickListener {
+                        Toast.makeText(this@MainActivity, "Option", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+        }
+
+        with(binding.transparentBackground) {
+            animate().alpha(0.9f).setDuration(300).setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    isClickable = true
+                }
+            })
+        }
+    }
+
+    private fun collapseFab() {
+        isExpanded = false
+        ObjectAnimator.ofFloat(binding.plusImageview, "rotation", 0f, -180f).start()
+        ObjectAnimator.ofFloat(binding.optionContainer, "translationY", 0f).start()
+
+        with(binding.optionContainer) {
+            animate().alpha(0f).setDuration(300).setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    isClickable = false
+                    setOnClickListener(null)
+                }
+            })
+        }
+
+        with(binding.transparentBackground) {
+            animate().alpha(0f).setDuration(300).setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    isClickable = false
+                }
+            })
         }
     }
 
@@ -50,7 +121,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
